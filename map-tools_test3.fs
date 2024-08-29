6e9ef66d53d994e66ce7a917c8443e92a2ad7994
Andrew <andrew81244@outlook.com>
2024-08-29 15:39:44 +0800

    Discussion on notation with UH

--- map-tools_test3.fs
+++ map-tools_test3.fs
@@ -11,7 +11,13 @@ CR
 	s" red" colourTable s" 0xff0000" |<=|
 	s" green" colourTable <=" 0x00ff00"
 	s" blue" colourTable <=" 0x0000ff"
+	
+\	s" blue" =>" 0x0000ff" colourTable 
+\  ( caddr u) thisImageMap =>" FOCUSER"    \ " FOCUSER" is standardized keyword
+\  ( compute-value caddr u) R@ ( get-myMap) =>" FOCUSER"    \ " FOCUSER" is standardized keyword
+\  ( "           " caddr u) R@ (     myMap) =>" FILTER" 
 
+													
 T{ s" red" colourTable >string hashS   }T s" 0xff0000" hashS ==
 T{ s" green" colourTable >string hashS }T s" 0x00ff00" hashS ==
 T{ s" blue" colourTable >number }T 255 ==
