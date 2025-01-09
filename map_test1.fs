bae9852e803279adc77190a7efb00d09ca1e039d
Andrew Read <andrew81244@outlook.com>
2025-01-09 18:52:17 +0100

    further testing

--- map_test1.fs
+++ map_test1.fs
@@ -6,17 +6,36 @@ include "%idir%\map.fs"
 CR
 Tstart
 
-	hex
-	map CONSTANT colourTable
-	s" red" colourTable >addr ff0000 swap !
-	s" green" colourTable >addr 00ff00 swap !
-	s" blue" colourTable >addr 0000ff swap !
+CR ." create a map and test some numeric key-value pairs" CR
+map CONSTANT colourTable1 hex
 
-T{ s" red" colourTable >addr @ }T ff0000 ==
-T{ s" blue" colourTable >addr @ }T 0000ff ==
-	s" blue" colourTable >addr 000099 swap !
-T{ s" blue" colourTable >addr @ }T 000099 ==
-	s" blue" colourTable >addr 0000ff swap !
-	
+s" red" colourTable1 >addr ff0000 swap !
+s" green" colourTable1 >addr 00ff00 swap !
+s" blue" colourTable1 >addr 0000ff swap !
+T{ s" red" colourTable1 >addr @ }T ff0000 ==
+T{ s" blue" colourTable1 >addr @ }T 0000ff ==
+	s" blue" colourTable1 >addr 000099 swap !
+T{ s" blue" colourTable1 >addr @ }T 000099 ==
+	s" blue" colourTable1 >addr 0000ff swap !
+decimal
+
+CR ." create a map and test some string key-value pairs" CR
+map CONSTANT colourTable2
+
+s" red" colourTable2 >addr 		s" ff0000" rot $!
+s" green" colourTable2 >addr 	s" 00ff00" rot $!
+s" blue" colourTable2 >addr 	s" 0000ff" rot $!
+T{ s" red" colourTable2 >addr $@ hashS }T s" ff0000" hashS  ==
+T{ s" blue" colourTable2 >addr $@ hashS }T s" 0000ff" hashS ==
+	s" blue" colourTable2 >addr s" 000099" rot $!
+T{ s" blue" colourTable2 >addr $@ hashS }T s" 000099" hashS ==
+	s" blue" colourTable2 >addr s" 0000ff" rot $!
+
+CR ." test with the map keys as Forth words in the input stream" CR
+colourTable2 +map
+T{ red hashS }T s" ff0000" hashS ==
+T{ blue hashS }T s" 0000ff" hashS ==
+colourTable2 -map
+
 CR
 Tend
